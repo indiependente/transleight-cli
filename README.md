65b9c8b3e391c2274b001960f6765c1df4a102ab
indiependente <rockerg991@gmail.com>
2014-08-13 19:46:22 +0200

    Create package.json.Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ transleight-cli
 ===============
 
 A CLI for transleight Language Translation Stream
+
+##Installation
+`npm install transleight-cli -g`
+
+##Usage
+After transleight-cli has been installed globally with npm -g, you can just open a terminal window and:
+
+- `transleight-cli -l` to take a look at the 64 available languages;
+- `transleight-cli lang` to start typing sentences in a language and getting them translated in the _lang_ language you passed as argument.
+
+##Example
+```
+$ transleight-cli it
+It's so simple to use!
+E ' così semplice da usare !
+```
+
+##License
+####MIT
--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var transleight = require('transleight');
 var lang = process.argv[2];
 
 if(!lang)
-	throw new Error('Usage: ./transleight-cli.js <targetlanguage>');
+	throw new Error('Usage: transleight-cli <targetlanguage>');
 if (lang.indexOf('-') == 0) {
 	switch (lang){
 		case '-l' : {console.log(transleight.supportedLangs());break;}
-		case '-h' : {console.log('transleight-cli.js\nA cli for transleight Language Translation Stream.\n\
-Usage:\n./transleight-cli.js <targetlanguage>\nOptions:\n-l : shows all the supported languages.\n-h : shows this menu\n');break;}
-		default : {console.log('Unknown option. Try ./transleight-cli.js -h for help.');}
+		case '-h' : {console.log('transleight-cli\nA cli for transleight Language Translation Stream.\n\
+Usage:\ntransleight-cli <targetlanguage>\nOptions:\n-l : shows all the supported languages.\n-h : shows this menu\n');break;}
+		default : {console.log('Unknown option. Try transleight-cli -h for help.');}
 	}
 }
 else
